6ccd0db43ecf4f985cd0593f7aca686d10978583
Alejandro Miranda <alejandroms@uoc.edu>
2021-01-05 00:15:12 +0100

    find paremeters in tb_functions return fix.count Orf by description method

--- orfManagement.py
+++ orfManagement.py
@@ -1,32 +1,44 @@
-
 import os
 import re
 
+
+def countOrfByDescription(path, description):
+    count = 0
+    with open(path) as orfFile:
+        for line in orfFile:
+            orfData = findFunctionProperties(line)
+            if orfData is not None and description in orfData[3]:
+                count += count
+    return count
+
+
+def findFunctionProperties(line):
+    """
+    Devuelve una lista con los parámetros de la linea del archivo de texto tb_functions
+    :param line: Texto con el formato adecuado para extraer las propiedades del orf
+    :return: una lista con el siguiente orden idClase,orf,nombre,description
+    """
+    if re.match("function(.*)", line) is not None:
+        # Primero recuperamos todos los parámetros de las línea
+        properties = re.search("\((.*)\)", line).group(1)
+        # Obtenemos la clase
+        classId = re.search("\[(\d,\d,\d,\d)\]", properties).group(1)
+        # Una vez obtenida la clase,la eliminamos y spliteamos el texto para sacar el resto de campos
+        properties = re.sub(",\[(\d,\d,\d,\d)\]", '', properties).split(",")
+
+        description = ""
+        # Creamos la description por si hubiera sido cortada en el paso anterior
+        for item in properties[2:]:
+            description = description + item
+
+        return classId, properties[0], properties[1], description
+
+
 def countOrf(dirPath):
-    orfCount=dict()
+    orfCount = dict()
 
-
-    def findFunctionProperties(line):
-        """
-        Devuelve una lista con los parámetros de la linea
-        :param line:
-        :return:
-        """
-        if re.match("function(.*)", line) is not None:
-            #function(tb186,[1,1,1,0],'bglS',"beta-glucosidase").
-            #orf,idClass,genName,description
-            #Primero recuperamos todos los parámetros de las líneas functions
-            properties=re.search("\((.*)\)", line).group(1)
-            #Obtenemos la clase
-            classId=re.search("\[(\d,\d,\d,\d)\]",properties).group(1)
-            #Una vez obtenida la clase,la eliminamos y spliteamos el texto para sacar el resto de campos
-            properties=re.sub(",\[(\d,\d,\d,\d)\]", '', properties).split(",")
-            #Finalmente volvemos a insertar la clase para crear una lista con todos los parámetros
-            properties.insert(0,classId)
-            return (properties)
-
-    def readAndCount(path,orfDict):
-        orfCountCopy=orfDict.copy()
+    def readAndCount(path, orfDict):
+        orfCountCopy = orfDict.copy()
         with open(path) as orfFile:
             for line in orfFile:
                 orfClass = findFunctionProperties(line)
@@ -39,15 +51,12 @@ def countOrf(dirPath):
                         orfCountCopy[classId] += 1
         return orfCountCopy
 
-
-    if(os.path.isdir(dirPath)):
+    if (os.path.isdir(dirPath)):
         for file in os.listdir(dirPath):
-            orfCount=readAndCount(file,orfCount)
+            return readAndCount(file, orfCount)
     else:
-        orfCount=readAndCount(dirPath,orfCount)
-
-    print(orfCount)
+        return readAndCount(dirPath, orfCount)
 
 
-
-countOrf("data/tb_functions_sample.pl")
+#print(countOrf("data/tb_functions_sample.pl"))
+print(countOrfByDescription("data/tb_functions_sample.pl", "Respiration"))
